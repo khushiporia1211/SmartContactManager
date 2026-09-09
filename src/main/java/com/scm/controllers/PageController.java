32079257f326d83b92d7e5e644afe17a18cb301f
khushi <khushiporia09@gmail.com>
2026-09-09 14:22:49 +0530

    Restore templates and static assets

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -20,4 +20,29 @@ public class PageController {
 
     }
 
+    @RequestMapping("/about")
+    public String aboutPage(){
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String servicePage(){
+        return "services";
+    }
+
+    @RequestMapping("/login")
+    public String userLogin(){
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @RequestMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
 }
